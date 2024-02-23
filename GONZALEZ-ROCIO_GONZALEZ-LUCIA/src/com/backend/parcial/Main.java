242734f3be8587b5bc38bf4dbe04e965faabb772
Lucía Gonzalez <luciagoq@gmail.com>
2024-02-22 23:29:40 -0300

    Creación DaoH2

--- GONZALEZ-ROCIO_GONZALEZ-LUCIA/src/com/backend/parcial/Main.java
+++ GONZALEZ-ROCIO_GONZALEZ-LUCIA/src/com/backend/parcial/Main.java
@@ -1,23 +1,29 @@
 package com.backend.parcial;
 
 import com.backend.parcial.dbconnection.H2Connection;
+import org.apache.log4j.Logger;
 
 import java.sql.Connection;
 import java.sql.ResultSet;
 import java.sql.Statement;
 
 public class Main {
-    public static void main(String[] args) throws Exception {
-        Connection con = H2Connection.getConnection();
-        Statement stmt = con.createStatement();
+    private static final Logger LOGGER = Logger.getLogger(Main.class);
+    public static void main(String[] args) {
+        try {
+            Connection con = H2Connection.getConnection();
+            Statement stmt = con.createStatement();
 
-        //Codigo para consultar todos los registros de la tabla TEST
-        String sql = "select * from TEST";
-        ResultSet rd = stmt.executeQuery(sql);
+            //Codigo para consultar todos los registros de la tabla TEST
+            String sql = "select * from TEST";
+            ResultSet rd = stmt.executeQuery(sql);
 
-        //Código para recorrer el resultado de la consulta
-        while(rd.next()) {
-            System.out.println(rd.getInt(1) + rd.getString(2));
+            //Código para recorrer el resultado de la consulta
+            while (rd.next()) {
+                System.out.println(rd.getInt(1) + rd.getString(2));
+            }
+        } catch (Exception e) {
+            LOGGER.error(e.getMessage());
         }
     }
 }
---
+++ GONZALEZ-ROCIO_GONZALEZ-LUCIA/src/com/backend/parcial/dao/impl/MedicamentoDaoMemory.java
@@ -0,0 +1,16 @@
+package com.backend.parcial.dao.impl;
+
+import com.backend.parcial.dao.IDao;
+import com.backend.parcial.entity.Odontologo;
+
+public class MedicamentoDaoMemory implements IDao<Odontologo> {
+    @Override
+    public Odontologo registrar(Odontologo odontologo) {
+        return null;
+    }
+
+    @Override
+    public Odontologo buscarPorId(int id) {
+        return null;
+    }
+}
